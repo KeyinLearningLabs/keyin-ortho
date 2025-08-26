5cd09c20f1149acde746b4debc45f3f8f6d66ee2
Alex <Alex.bristow@keyin.com>
2025-08-26 16:29:10 -0230

    changes to ui

--- src/components/IndividualProgram.tsx
+++ src/components/IndividualProgram.tsx
@@ -1,20 +1,20 @@
 const IndividualProgram = () => {
   return (
     <>
-      <div className="flex flex-col items-center mx-5">
-        <h1 className="mt-20 text-center text-keyin-bright-blue">Program Delivery</h1>
-        <p className="mb-6 text-xl text-center">
+      <div className="flex flex-col mx-5">
+        <h1 className="mt-20 text-keyin-bright-blue">Program Delivery</h1>
+        <p className="mb-6 text-xl">
           The theory portion of the program is delivered as a guided, self-study course via our e-Class platform. This
           online, self-paced format allows participants to progress through the content on their own schedule, with no
           in-person attendance required. Learners will have access to all digital materials for one full year from the
           date of enrollment, providing flexibility and ample time for review and preparation.
         </p>
-        <p className="mb-6 text-xl text-center">
+        <p className="mb-6 text-xl">
           After passing the written theory exam, students become eligible to register for the in-person clinical
           component. This hands-on training session is delivered in a supervised clinical environment, where
           participants will develop and demonstrate essential orthodontic assisting skills.
         </p>
-        <p className="text-xl text-center">
+        <p className="text-xl">
           Theory Exams are held on-line each Friday 9:00 – 12:00. The exam must be completed a minimum of three weeks
           before attending the in-person clinical component. Once you pass, you are assigned to the next available
           clinical portion. It is strongly recommended that students begin the theory component at least three months
